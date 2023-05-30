190ef308236472ecdeef1a762c94a6ab8a3ae983
vadim_karnetov <karnetov@evolenta.ru>
2023-05-30 12:10:48 +0300

    Создание Разметки под лист диалогов

---
+++ app/src/main/java/com/youranemone/noticeboard/chat/ChatListActivity.java
@@ -0,0 +1,16 @@
+package com.youranemone.noticeboard.chat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.youranemone.noticeboard.R;
+
+public class ChatListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_list);
+    }
+}
