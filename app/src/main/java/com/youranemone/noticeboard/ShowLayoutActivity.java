c9d4b5b8b3f657375ae8a4d86bb49591329d275c
hp13r <hp13rus@mail.ru>
2023-06-03 16:19:30 +0300

    Реализована возможность позвонить по нажатию на кнопку

--- app/src/main/java/com/youranemone/noticeboard/ShowLayoutActivity.java
+++ app/src/main/java/com/youranemone/noticeboard/ShowLayoutActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -124,6 +125,14 @@ public class ShowLayoutActivity extends AppCompatActivity {
                     i.putExtra(MyConstants.RECEIVER_IMAGE_ID,creatorAvatar);
                     startActivity(i);
                 });
+            }
+        });
+        callBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("tel:" + tvTelephone.getText().toString());
+                Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+                startActivity(intent);
             }
         });
     }
